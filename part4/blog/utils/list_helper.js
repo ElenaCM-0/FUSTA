491a4b3c9e717059656714d7ac47981d6aa7dc51
ElenaCM-0 <elena.carrera@estudiante.uam.es>
2025-05-14 16:54:32 +0300

    Until 4.5

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -2,6 +2,25 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+    return blogs.reduce((total, cur_element) => {
+        return total + cur_element.likes
+    },  0)
+}
+
+const favouriteBlog = (blogs) => {
+    if (blogs.length == 0) {
+      return []
+    }
+    
+    return blogs.reduce((cur_max, cur_element) => {
+        if (cur_element.likes > cur_max.likes) {
+          return cur_element
+        }
+        return cur_max
+    },  blogs[0])
+}
+
 module.exports = {
-  dummy
+  dummy, totalLikes, favouriteBlog
 }
